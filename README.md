5f74620f0d00e232c31350befcd6a6a985b40cb1
joshuaparker <joshuanparker@gmail.com>
2020-06-10 22:54:45 -0400

    I've created the table and imported csv into database.

--- README.md
+++ README.md
@@ -1,3 +1,4 @@
 # database_python3
 a database in python3
 I deleted the other repo because I uploaded bank info. 
+Wasn't my best moment but I made a database that I can load my csv into. 
--- build_db.py
+++ build_db.py
@@ -1,10 +1,17 @@
-import sqlite3, csv
+import sqlite3, csv, datetime
 
 
 # TODO: Build tables if there are none
 db = sqlite3.connect("transactions.sqlite", detect_types=sqlite3.PARSE_DECLTYPES)
-db.execute("CREATE TABLE IF NOT EXISTS transactions (account_number TEXT, account_type TEXT, account_name TEXT, date DATE, "
-           "transaction_info TEXT, transaction_type TEXT, ammount REAL, balance REAL)")
+db.execute("CREATE TABLE IF NOT EXISTS transactions "
+           "(account_number TEXT, "
+           "account_type TEXT, "
+           "account_name TEXT, "
+           "date DATE, "
+           "transaction_info TEXT, "
+           "transaction_type TEXT, "
+           "amount REAL, "
+           "balance REAL)")
 
 cursor = db.cursor()
 # TODO: import csv data into application
@@ -12,10 +19,29 @@ cursor = db.cursor()
 #  but then could that be a vulnerability,
 #  better to import the data and verify its valid data. . .
 
-fname =
+
+fname = "bnk-prod.csv"
+
 # TODO: export data to database
+with open(fname) as csv_file:
+    csv_reader = csv.reader(csv_file, delimiter=',')
+    for row in csv_reader:
+        account_number = row[0]
+        account_type = row[1]
+        account_name = row[2]
+        date = row[3]
+        transaction_info = row[4]
+        transaction_type = 'unpack Tuple'
+        amount = row[8]
+        balance = row[9]
+        print(row)
+        cursor.execute('''INSERT INTO transactions(account_number, account_type, account_name, date, transaction_info, transaction_type, amount, balance)
+        VALUES(?,?,?,?,?,?,?,?)''', (account_number, account_type, account_name, date, transaction_info, transaction_type, amount, balance))
+        db.commit()
+
+
 
 # TODO: create functions that run computations from data in database
 
-# TODO: generate reports on data
+# TODO: generate reports on data in graphs
 
